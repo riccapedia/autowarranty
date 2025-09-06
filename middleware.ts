1b51928c0e916d15fd0d1a494328fc78e3a31e98
riccapedia <73568233+riccapedia@users.noreply.github.com>
2025-09-05 22:53:40 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
+export default clerkMiddleware({
   publicRoutes: ["/sign-in(.*)", "/sign-up(.*)", "/favicon.ico"],
   signInUrl: "/sign-in",
 });
 
 export const config = {
+  // run on everything except static files and _next
   matcher: ["/((?!_next|.*\\..*).*)"],
 };
